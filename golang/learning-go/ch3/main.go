dc3bb81c98badb0b21768ab9c6483fb08bbbe35f
Ammar Lakis <ammar.lakis@gmail.com>
2025-02-27 10:27:39 +0100

    commit

---
+++ golang/learning-go/ch3/main.go
@@ -0,0 +1,93 @@
+package main
+
+func main() {
+	// // var x [10]int = [10]int{1, 2, 5: 10}
+	// var array [2]int = [2]int{9, 10}
+	// var slice []int
+	// fmt.Println("Hello, ", array, slice, slice == nil, nil)
+	// slice = append(slice, 1, 2, 3)
+	// fmt.Println(slice, slice == nil)
+	// fmt.Println(append(slice, []int{4, 5}...))
+
+	// var x []int
+	// fmt.Println(x, len(x), cap(x))
+	// x = append(x, 28)
+	// fmt.Println(x, len(x), cap(x))
+	// x = append(x, 28)
+	// fmt.Println(x, len(x), cap(x))
+	// x = append(x, 28)
+	// fmt.Println(x, len(x), cap(x))
+	// x = append(x, 28)
+	// fmt.Println(x, len(x), cap(x))
+
+	// x := make([]int, 6, 7)
+	// fmt.Println(x[5])
+	// fmt.Println(x, len(x), cap(x))
+	// x = append(x, 1)
+	// fmt.Println(x, len(x), cap(x))
+	// x = append(x, 1)
+	// fmt.Println(x, len(x), cap(x))
+
+	// x := []int{1, 2, 3, 4, 5}
+	// fmt.Println(x, len(x), cap(x))
+
+	// x := []int{1, 2, 3, 4, 5}
+	// fmt.Println(x[:])
+	// fmt.Println(x[1:2])
+	// fmt.Println(x[1:])
+	// fmt.Println(x[0:])
+	// fmt.Println(x[:1])
+
+	// x := []int{1, 2, 3}
+	// y := x[1:]
+	// y[0] = 10
+	// fmt.Println(x)
+
+	// x := []int{1, 2, 3, 4, 5}
+	// y := x[1:3]
+	// y = append(y, 10)
+	// fmt.Println(x)
+	// fmt.Println(y)
+
+	// x := make([]int, 0, 5)
+	// x = append(x, 1, 2, 3, 4)
+	// y := x[:2]
+	// z := x[2:]
+	// fmt.Println(cap(x), cap(y), cap(z))
+	// y = append(y, 30, 40, 50)
+	// x = append(x, 60)
+	// z = append(z, 70)
+	// fmt.Println("x ", x)
+	// fmt.Println("y ", y)
+	// fmt.Println("z ", z)
+
+	// x := []int{1, 2, 3}
+	// y := make([]int, 4)
+	// num := copy(y, x)
+	// fmt.Println(y, num)
+
+	// s := "abcd"
+	// another := []byte("abcd")
+	// fmt.Println(s)
+	// fmt.Println(another)
+
+	// x := []int{1, 2, 3}
+	// fmt.Println(x[3:])
+	// fmt.Println(len(x))
+
+	// type node struct {
+	// 	val int
+	// 	next *node
+	// }
+
+	// var x *node = &node{}
+	// x.val = 10
+
+	// var x []int = []int{}
+	// x = append(x, 10)
+	// x = append(x, 9)
+	// x = append(x, 8)
+	// x = append(x, 7)
+	// fmt.Println(x)
+
+}
